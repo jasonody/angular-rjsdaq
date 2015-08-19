28dc6c1a9eef26680e125b1faf5633b7d1e32439
Jason Ody <jasonody@users.noreply.github.com>
2015-08-18 22:18:28 -0700

    Added change indicator to Security directive

--- client/src/components/security/securityController.js
+++ client/src/components/security/securityController.js
@@ -1,13 +1,45 @@
 export default class securityController {
 	
-	constructor() {
+	constructor($scope) {
+		
+		this._priceHistory = [];
+		this.changeText = '';
+		this.changePercentage = 0.0;
+		
+		this._init($scope);
+	}
+	
+	_init($scope) {
+		
+		$scope.$watch(
+			(scope) => { return this.security.price },
+			(newValue, oldValue, scope) => { 
+				
+				this._priceHistory.push(this.security.price);
+				this._updateChange(); 
+			}
+		);
+	}
+	
+	_updateChange() {
+		
+		if (this._priceHistory.length > 1) {
+			var newPrice = this._priceHistory[this._priceHistory.length - 1];
+			var previousPrice = this._priceHistory[this._priceHistory.length - 2];
 
+			var priceChange = (((newPrice - previousPrice) / previousPrice) *100).toFixed(1);
+			var priceChangeSign = priceChange > 0 ? '+' : '';
+
+			this.changeText = `${priceChangeSign}${priceChange}%`;
+			this.changePercentage = priceChange;
+		}
 	}
 	
 	getPriceString() {
 
 		return this.security.price == null ? '-' : this.security.price + '¢';
 	}
+	
 }
 
-securityController.$inject = [];
+securityController.$inject = ['$scope'];
