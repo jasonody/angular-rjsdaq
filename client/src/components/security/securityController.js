62da644d1430b1014bfdd09beeb2c4e37cc19d74
Jason Ody <jasonody@users.noreply.github.com>
2015-08-21 19:26:44 -0700

    Added price graph to Security directive

--- client/src/components/security/securityController.js
+++ client/src/components/security/securityController.js
@@ -5,6 +5,7 @@ export default class securityController {
 		this._priceHistory = [];
 		this.changeText = '';
 		this.changePercentage = 0.0;
+		this.priceHistoryGraphGraph = [];
 		
 		this._init($scope);
 	}
@@ -16,23 +17,54 @@ export default class securityController {
 			(newValue, oldValue, scope) => { 
 				
 				this._priceHistory.push(this.security.price);
-				this._updateChange(); 
+				this._updateChange();
+				this._updatePriceHistoryGraph();
 			}
 		);
 	}
 	
 	_updateChange() {
 		
-		if (this._priceHistory.length > 1) {
-			var newPrice = this._priceHistory[this._priceHistory.length - 1];
-			var previousPrice = this._priceHistory[this._priceHistory.length - 2];
+		var newPrice = this._priceHistory[this._priceHistory.length - 1];
+		var previousPrice = this._priceHistory[this._priceHistory.length - 2];
 
-			var priceChange = (((newPrice - previousPrice) / previousPrice) *100).toFixed(1);
-			var priceChangeSign = priceChange > 0 ? '+' : '';
+		var priceChange = (((newPrice - previousPrice) / previousPrice) *100).toFixed(1);
+		var priceChangeSign = priceChange > 0 ? '+' : '';
 
-			this.changeText = `${priceChangeSign}${priceChange}%`;
-			this.changePercentage = priceChange;
-		}
+		this.changeText = `${priceChangeSign}${priceChange}%`;
+		this.changePercentage = priceChange;
+	}
+	
+	_updatePriceHistoryGraph() {
+		
+		var minPrice, maxPrice;
+		var priceHistory = this._priceHistory;
+
+		priceHistory.forEach(function (price) {
+
+			if (minPrice == null || minPrice > price) {
+				minPrice = price;
+			}
+			if (maxPrice == null || maxPrice < price) {
+				maxPrice = price;
+			}
+		});
+
+		var delta = maxPrice - minPrice;
+
+		this.priceHistoryGraph = priceHistory.map(function (price, i) {
+
+			var height = delta === 0 ? 100 : 10;
+
+			if (delta > 0) {
+				height += 90 * (1 - ((maxPrice - price) / delta))
+			}
+
+			return {
+				price,
+				height
+			};
+		});
 	}
 	
 	getPriceString() {
